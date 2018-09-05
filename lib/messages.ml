3ffcb950da4df7482add88dbbcf55dfc24c67156
sam <nullvoidnil@mail.com>
2018-09-05 04:52:46 -0400

    merging

---
+++ lib/messages.ml
@@ -0,0 +1,312 @@
+open Frame
+open Util
+       
+       
+
+module Status = struct
+  type t = [
+
+    | `Ok
+    | `Nack
+    | `Error
+    ]
+
+  let to_int =
+    function
+    | `Ok -> 0
+    | `Nack -> 1
+    | `Error -> 2
+
+  let of_int =
+    function
+    | 0 -> `Ok 
+    | 1 -> `Nack
+    | 2 -> `Error
+    | _ -> `Unknown
+
+            
+          
+end
+
+
+module TREQ = struct
+  type t = {tag: int32; path: string list; headers: Headers.t; body: Cstruct.t}
+
+  let tag t = t.tag
+                
+  let path t = t.path
+
+  let headers t =
+    t.headers
+
+  let body t = t.body
+
+  let add_headers t h =
+    let headers = t.headers @ h in
+    {t with headers}
+
+  let set_headers t h =
+    {t with headers = h;}
+
+
+
+  let buffer_size t =
+    let path_string = String.concat "/" t.path in
+    let hsize = Headers.buffer_size t.headers in
+    let body_size = Cstruct.len t.body in
+
+    let size = 4 + (String.length path_string) + hsize + body_size in 
+    size
+      
+    
+
+
+
+
+  let to_body t =
+    let size = buffer_size t in
+    let len = Int32.of_int size in
+    
+    let path = String.concat "/" t.path in
+    let slen = String.length path in
+    
+    let buf = Cstruct.create size in
+    let bsize = Cstruct.len t.body in
+
+
+    
+    let _ = 
+      write len 4 Cstruct.BE.set_uint32 buf
+      |> Headers.write t.headers 
+      |> write slen 2 Cstruct.BE.set_uint16 
+      |> write_string path
+               
+      |> (fun c ->
+        
+        Cstruct.blit t.body 0 c 0 bsize;
+        Cstruct.shift c bsize
+      )
+
+    in buf
+
+  let of_frame f =
+
+    let tag = Frame.tag f in
+    let body = Frame.body f in 
+    
+    let (headers, b1) = Headers.read body in
+    let (path_l, b2) = read 2 Cstruct.BE.get_uint16 b1 in
+    let (path_s, b3) =  read_string path_l b2 in 
+
+    let body =
+      let len = Cstruct.len b3 in 
+      Cstruct.sub b3 0 len
+    in
+
+    let path = Str.split (Str.regexp "/") path_s in
+    
+    
+    {tag; path; headers; body}
+
+         
+    
+                 
+
+                 
+end
+
+module RREQ = struct
+  type t = {tag: int32; status: int; headers: Headers.t; body: Cstruct.t}
+
+  let buffer_size t =
+    1 + 4 + (Headers.buffer_size t.headers) +  (Cstruct.len t.body)
+
+
+
+  let tag t = t.tag
+
+  let status t = t.status
+
+  let headers t = t.headers
+
+  let body t = t.body
+
+                 
+                    
+
+  let to_body t =
+    let size =  buffer_size t in
+    let len = Int32.of_int size in
+    
+    let buf = Cstruct.create size in
+    let body_s = Cstruct.len t.body in
+
+
+    let _ = 
+      write len 4 Cstruct.BE.set_uint32 buf
+      |> Headers.write t.headers 
+      |> write t.status 1 Cstruct.set_uint8 
+      |> (fun c ->
+        
+        Cstruct.blit t.body 0 c 0 body_s;
+        Cstruct.shift c body_s
+      )
+    in
+    buf
+
+  let of_frame f =
+    let tag = Frame.tag f in
+    let (headers, b1) = Headers.read f.body in
+    let (status, b2) = read 1 Cstruct.get_uint8 b1 in
+
+    let body =
+      let len = Cstruct.len b2 in 
+      Cstruct.sub b2 0 len
+    in
+    {tag; status; headers; body}
+
+         
+         
+end 
+
+module INIT = struct
+  type t = {tag: int32; headers: Headers.t;}
+
+  let to_body t =
+    Headers.to_cstruct t.headers
+
+  let of_frame f =
+    let tag = Frame.tag f in
+    let headers = Frame.body f |> Headers.of_cstruct in
+    {tag; headers}
+
+  let tag t = t.tag
+                
+  let headers t = t.headers
+                    
+  let set_headers t headers =
+    {t with headers}
+
+  let add_headers t h =
+    let headers = t.headers @ h in
+    {t with headers}
+    
+
+  let set_headers t h =
+    {t with headers = h;}
+
+
+                       
+end
+
+                
+
+type t = [
+
+  | `TPING of int32
+
+  | `RPING of int32 
+
+  | `TINIT of INIT.t
+  | `RINIT of INIT.t
+
+  | `TREQ of TREQ.t
+  | `RREQ of RREQ.t
+
+  | `TCLOSE of int32
+  | `RCLOSE of int32
+                 
+
+  ]
+
+let tag t =
+  function 
+  | `TPING i -> i
+  | `RPING i -> i
+
+  | `TINIT x -> INIT.tag x
+  | `RINIT x -> INIT.tag x
+  | `TREQ x -> TREQ.tag x
+                        
+  | `RREQ x -> RREQ.tag x
+
+  | `TCLOSE i -> i
+  | `RCLOSE i -> i
+                   
+
+
+     
+let to_frame t =
+  function
+
+  | `TPING tag ->
+     let mtype = `TPING in 
+     {mtype; tag; body=Cstruct.empty}
+
+
+  | `RPING tag ->
+     let mtype = `RPING in
+     {mtype; tag; body=Cstruct.empty}
+
+  | `TINIT init ->
+     let body = INIT.to_body init in
+     let mtype = `TINIT in
+     let tag = INIT.tag init in 
+     {mtype; tag; body;}
+
+  | `RINIT init ->
+     let body = INIT.to_body init in
+     let mtype = `RINIT in
+     let tag = INIT.tag init in 
+     {mtype; tag; body;}
+       
+
+  | `TREQ t ->
+     let body = TREQ.to_body t in
+     let mtype = `TREQ in
+     let tag = TREQ.tag t in
+     {body; tag; mtype}
+
+  | `RREQ t ->
+     let body = RREQ.to_body t in
+     let mtype = `RREQ in
+     let tag = RREQ.tag t in
+     {body; tag; mtype}
+
+
+  | `TCLOSE i ->
+     let mtype = `TCLOSE in
+     let tag = i in
+     let body = Cstruct.empty in
+
+     {mtype; tag; body}
+
+  | `RCLOSE i ->
+     let mtype = `RCLOSE in
+     let tag = i in
+     let body = Cstruct.empty in
+
+     {mtype; tag; body}
+       
+       
+
+
+
+
+let of_frame f =
+  let tag = Frame.tag f in
+  let mtype = Frame.mtype f in 
+  match mtype with
+  | `TPING -> `TPING tag
+  | `RPING -> `RPING tag
+
+  | `TINIT -> `TINIT (INIT.of_frame f)
+  | `RINIT -> `RINIT (INIT.of_frame f)
+
+  | `TREQ -> `TREQ (TREQ.of_frame f)
+  | `RREQ -> `RREQ (RREQ.of_frame f)
+
+  | `TCLOSE -> `TCLOSE tag
+  | `RCLOSE -> `RCLOSE tag 
+               
+               
---
+++ spec.md
@@ -0,0 +1,193 @@
+# Protocol Spec
+
+
+## Summary
+Weave is a bidirectional session layer multiplexing protocol similar to ipfs's multistream, uber's TChannel, and twitter's mux protocol. 
+
+
+## Use cases
+There are a lot of complicated things going on in distributed systems, that are pushed into the application layer that shouldn't be such as service discovery, routing, liveness checking, distributed authentication, tracing etc, the session layer is a better place to put things.
+
+Connection Establishment is expensive, thus sharing connections is useful in many cases.
+
+Routing RPC requests.
+Being able to choose whether to perform requests based off of certain metrics.
+
+Implementing distributed algorithms in layer 5, so you users at layer 7 don't have to worry about it, such as gossip protocols, job control, replication, routing, load balancing etc.  
+
+Running different protocols on a single socket.
+
+Trivial to implement http on top of it, in order to gain things like connection streaming and multiplexing since ocaml doesn't have http2 yet.
+
+Eliminating HOL
+
+
+
+
+## Notation 
+
+
+
+Let field[c] be a field who's contents are a repeat sequence of size c.
+
+Let field:s be a field that takes s bytes.
+
+Let field-s be a field who's contents are of size s represented as an unsigned integer of size bytes. ie size:4 body:size 
+
+Note all integers are unsigned. 
+
+
+
+
+## Frame
+
+All messages are embedded into this format.
+size:4 type:1 version:2 tag:4 body-4
+
+
+Here example of how it's used.
+TINIT = size:4 0x03:1 tag:4  count:2 {key-2 value-2} [count]
+
+
+
+
+## Headers
+
+count:2 (key-2, value-2)[count]
+
+Headers are a utf8 key value pairs
+
+They are used for a variety of things in the protocol such as,
+negotiating content type, tls, embedding routing information, etc .
+
+It's prepended by a 16 bit int representing the number of pairs. 
+
+Each key value string is prepended by a 16 bit integer, to denote length.
+
+
+
+
+
+
+
+
+## Message Types
+Messages are distinguished by T and R which denote transmitter and receiver.
+RMSGs are matched by their corresponding TMSGS by their corresponding tag.
+
+Both client and server can send messages of any kind. 
+
+
+### TPING
+size:4 0x01:1 tag:4 
+Used to check liveness of peer.
+
+### RPING
+size:4 0x02:1 tag:4
+Reply to tping.
+
+
+### TINIT
+size:4 0x03:1 version:2 tag:4 
+
+Clients usually send this to begin an exchange.
+In the future it will also be used to negotiate versions.
+The headers are used to negotiate things like tls parameters, or request deadlines.
+
+It can be used for auth before an exchange begins. 
+You can't send any messages before you recieve the corresponding RINIT. 
+
+
+
+### RINIT
+
+size:4 0x03:1 version:2 tag:4 (key-2 value-2)[count]
+Response for TINIT.
+
+
+### TREQ
+size:4 0x04:1 version:2 tag:4 count:2 {key-2 value-2}[count] path-2 body-4
+
+Carries headers,followed by a logical destination represented as a utf8 encoded string, and the request body.
+
+The destination can be used to route requests.
+
+For Example
+```
+dev/dns/myapp/mydomain/com
+ipc/procs/workers/$
+
+ip/10.1.120.6
+dc/nyc/db_cluster
+
+rpc/calc/add
+```
+
+### RREQ
+size:4 0x05 version:2 tag:4 {key-2 value-2}[count] status:1 body-4
+
+Replies to a TREQ
+
+Carries headers, a status code represented as a 1 bit integer, and a string containing the reason for the error.
+
+
+
+Status codes are as follows
+```
+0 = OK request succesful.
+1 = ERROR generic
+2 = NACK didn't even attempt request.
+```
+
+
+
+
+
+### TCLOSE
+size:4 0x06:1 version:2 tag:4 
+Tells the other side to close session. 
+
+
+### RCLOSE
+size:4 0x06 version:2 tag:4 
+Acknowledges TCLOSE and does it.
+
+
+
+
+
+## Workflows
+
+
+Request response initiated by client
+```
+
+  TINIT -> Server
+  RINIT -> Client
+  
+  TREQ -> Server
+  RREQ -> Client
+  
+  TCLOSE -> Server
+  RCLOSE -> Client
+
+```
+
+
+
+Request response initiated by server
+```
+  let Client = Server.conns.get(peer)
+
+  TINIT -> Client
+  RINIT -> Server
+
+  TREQ -> Client
+  RREQ -> Server
+
+  TCLOSE -> Client
+  RCLOSE -> Server
+
+
+```
+
